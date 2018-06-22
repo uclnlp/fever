7f57886128d351f8423edaae8ebf025fca122c58
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2018-06-22 16:41:46 +0100

    remove unnecessary functions and yapfify

--- jack_reader.py
+++ jack_reader.py
@@ -8,7 +8,9 @@ from jack.core import QASetting
 
 def read_ir_result(path):
     instances = read_jsonl(path)
-    t2jnum = titles_to_jsonl_num(wikipedia_dir=abs_path("data/wiki-pages/wiki-pages/"), doctitles=abs_path("data/doctitles"))
+    t2jnum = titles_to_jsonl_num(
+        wikipedia_dir=abs_path("data/wiki-pages/wiki-pages/"),
+        doctitles=abs_path("data/doctitles"))
     titles = list()
 
     # make list of titles
@@ -19,28 +21,20 @@ def read_ir_result(path):
     t2l2s = load_doclines(titles, t2jnum)
 
     for instance in instances:
-        instance["evidence"] = get_evidence_sentence(instance["predicted_sentences"], t2l2s)
+        instance["evidence"] = get_evidence_sentence(
+            instance["predicted_sentences"], t2l2s)
 
     return instances
 
 
-def predict(instances, t2jnum):
-    """
-    instances: each lines of dev.sentences.p5.s5.jsonl
-    """
-    pred = list()
-    for instance in instances:
-        evidence_sentences = get_evidence_sentence(instance["evidence"], t2jnum)
-        nli_setting = QASetting(question=instance["claim"], support=[evidence_sentences])
-        prediction = dam_reader([nli_setting])
-        pred.append({"actual": instance["label"], "predicted": prediction[0][0].text})
-    return pred
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("read claim/evidence and output verdict")
-    parser.add_argument("in_file", help="input file path for rte (e.g., dev.sentences.p5.s5.jsonl)")
+    parser.add_argument(
+        "in_file",
+        help="input file path for rte (e.g., dev.sentences.p5.s5.jsonl)")
     parser.add_argument("out_file", help="output prediction file")
-    parser.add_argument("--saved_reader", help="path to saved reader directory")
+    parser.add_argument(
+        "--saved_reader", help="path to saved reader directory")
     args = parser.parse_args()
 
     pred = list()
@@ -51,5 +45,10 @@ if __name__ == "__main__":
         # question: hypothesis, support: [premise]
         nli_setting = QASetting(question=claim, support=[evidence])
         prediction = dam_reader([nli_setting])
-        pred.append({"actual": instance["label"], "predicted": convert_label(prediction[0][0].text, inverse=True)})
+        pred.append({
+            "actual":
+            instance["label"],
+            "predicted":
+            convert_label(prediction[0][0].text, inverse=True)
+        })
     save_jsonl(pred, abs_path(args.out_file))
