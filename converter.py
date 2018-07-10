ea12ba76a9af54f8fd2ef1a303138c989c4f36c5
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2018-07-10 15:54:44 +0100

    add rational filtering for use_ir_pred

--- converter.py
+++ converter.py
@@ -5,6 +5,7 @@ import os
 import re
 import argparse
 import json
+from collections import Counter
 from tqdm import tqdm
 from util import abs_path
 from fever_io import titles_to_jsonl_num, load_doclines, read_jsonl, save_jsonl, get_evidence_sentence_list
@@ -36,8 +37,42 @@ def snli_format(id, pair_id, label, evidence, claim):
         "sentence2": claim
     }
 
+def sampling(converted_instances):
+    """to avoid imbalance of data, sample nei labels according to the # of non_nei labels
+    ["NEI", "NEI", "SUP", "SUP", "NEI"]
+    ->
+    ["NEI", "NEI", "SUP", "SUP"]
+    should be refactored
+    """
+    filtered_labels = list()
+    for instance in converted_instances:
+        filtered_labels.append(instance["gold_label"])
 
-def _convert_instance(instance, t2l2s, prependlinum, prependtitle, use_ir_prediction):
+    n_neis = Counter(filtered_labels)[convert_label("NOT ENOUGH INFO")]
+    n_instances = len(converted_instances)
+    n_samples = (n_instances - n_neis)
+
+    if n_samples == 0:
+        return converted_instances
+
+    sampled_instances = list()
+    for filtered_label, instance in zip(filtered_labels, converted_instances):
+        if filtered_label != convert_label("NOT ENOUGH INFO"):
+            sampled_instances.append(instance)
+
+    sample_count = 0
+    for filtered_label, instance in zip(filtered_labels, converted_instances):
+        if filtered_label != convert_label("NOT ENOUGH INFO"):
+            continue
+        sampled_instances.append(instance)
+        sample_count += 1
+        if sample_count == n_samples:
+            break
+
+    return sampled_instances
+
+
+def _convert_instance(instance, t2l2s, prependlinum, prependtitle, use_ir_prediction, n_sentences):
     """convert single instance to either one or multiple instances
     Args
     instance: instance of FEVER dataset.
@@ -54,12 +89,12 @@ def _convert_instance(instance, t2l2s, prependlinum, prependtitle, use_ir_predic
     converted_instances = list()
     # assert instance["evidence"] == [[[hoge, hoge, title, linum], [hoge, hoge, title, linum]], [[..],[..],..], ...]
     if use_ir_prediction:
-        evidence_linum = [[title, linum] for title, linum in instance["predicted_sentences"]
+        evidence_linum = [[title, linum] for title, linum in instance["predicted_sentences"][:n_sentences]
                           if title in t2l2s]
         contained_flags = compare_evidences(instance["evidence"], evidence_linum)
 
         for eidx, ((title, linum), contained) in enumerate(zip(evidence_linum, contained_flags)):
-            label = instance["label"] if instance["label"] != "NOT ENOUGH INFO" and contained else "NOT ENOUGH INFO"
+            label = instance["label"] if ( instance["label"] != "NOT ENOUGH INFO" and contained ) else "NOT ENOUGH INFO"
 
             converted_instances.append(
                 snli_format(
@@ -70,6 +105,8 @@ def _convert_instance(instance, t2l2s, prependlinum, prependtitle, use_ir_predic
                         get_evidence_sentence_list(
                             [(title, linum)], t2l2s, prependlinum=prependlinum, prependtitle=prependtitle)),
                     claim=instance["claim"]))
+
+        converted_instances = sampling(converted_instances)
 
     else:
         for eidx, evidence_set in enumerate(instance["evidence"]):
@@ -92,7 +129,7 @@ def _convert_instance(instance, t2l2s, prependlinum, prependtitle, use_ir_predic
     return converted_instances
 
 
-def convert(instances, prependlinum=False, prependtitle=False, use_ir_prediction=False):
+def convert(instances, prependlinum=False, prependtitle=False, use_ir_prediction=False, n_sentences=5):
     """convert FEVER format to jack SNLI format
     Arg
     instances: list of dictionary of FEVER format
@@ -115,7 +152,7 @@ def convert(instances, prependlinum=False, prependtitle=False, use_ir_prediction
             instance["evidence"] = evidences
 
         if use_ir_prediction:
-            titles = [title for title, _ in instance["predicted_sentences"]]
+            titles = [title for title, _ in instance["predicted_sentences"][:n_sentences]]
         else:
             titles = [
                 title for evidence_set in instance["evidence"]
@@ -133,7 +170,7 @@ def convert(instances, prependlinum=False, prependtitle=False, use_ir_prediction
     for instance in tqdm(instances, desc="conversion"):
         converted_instances.extend(
             _convert_instance(
-                instance, t2l2s, prependlinum=prependlinum, prependtitle=prependtitle, use_ir_prediction=use_ir_prediction))
+                instance, t2l2s, prependlinum=prependlinum, prependtitle=prependtitle, use_ir_prediction=use_ir_prediction, n_sentences=n_sentences))
 
     return converted_instances
 
@@ -143,19 +180,22 @@ if __name__ == "__main__":
     parser.add_argument("src")
     parser.add_argument("tar")
     parser.add_argument("--use_ir_pred", action="store_true")
+    parser.add_argument("--n_sentences", default=5, type=int)
     parser.add_argument("--prependlinum", action="store_true")
     parser.add_argument("--prependtitle", action="store_true")
     parser.add_argument("--convert_test", action="store_true")
     # parser.add_argument("--testset", help="turn on when you convert test data", action="store_true")
     args = parser.parse_args()
     print(args)
+    if args.n_sentences!= 5 and not args.use_ir_pred:
+        raise RuntimeError("parameter n_sentences must be used with use_ir_pred flag")
 
     if args.convert_test:
         test_in = '''[{"id": 15812, "verifiable": "VERIFIABLE", "label": "REFUTES", "claim": "Peggy Sue Got Married is a Egyptian film released in 1986.", "evidence": [[[31205, 37902, "Peggy_Sue_Got_Married", 0], [31205, 37902, "Francis_Ford_Coppola", 0]], [[31211, 37908, "Peggy_Sue_Got_Married", 0]]], "predicted_pages": ["Peggy_Sue_Got_Married_-LRB-musical-RRB-", "Peggy_Sue_Got_Married_-LRB-song-RRB-", "Peggy_Sue_Got_Married", "Peggy_Sue", "Peggy_Sue_-LRB-band-RRB-"], "predicted_sentences": [["Peggy_Sue_Got_Married", 0], ["Peggy_Sue_Got_Married_-LRB-musical-RRB-", 0], ["Peggy_Sue_Got_Married_-LRB-song-RRB-", 0], ["Peggy_Sue", 0], ["Peggy_Sue_Got_Married_-LRB-musical-RRB-", 2]]}, {"id": 229289, "verifiable": "NOT VERIFIABLE", "label": "NOT ENOUGH INFO", "claim": "Neal Schon was named in 1954.", "evidence": [[[273626, null, null, null]]], "predicted_pages": ["Neal_Schon", "Neal", "Named", "Was_-LRB-Not_Was-RRB-", "Was"], "predicted_sentences": [["Neal_Schon", 0], ["Neal_Schon", 6], ["Neal_Schon", 5], ["Neal_Schon", 1], ["Neal_Schon", 2]]}, {"id": 15711, "verifiable": "VERIFIABLE", "label": "SUPPORTS", "claim": "Liverpool F.C. was valued at $1.55 billion at one point.", "evidence": [[[31112, 37788, "Liverpool_F.C.", 11]]], "predicted_pages": ["Liverpool_F.C.", "Liverpool_F.C._-LRB-Montevideo-RRB-", "Liverpool_F.C._-LRB-Superleague_Formula_team-RRB-", "Liverpool_F.C._-LRB-disambiguation-RRB-", "Liverpool"], "predicted_sentences": [["Liverpool_F.C.", 11], ["Liverpool", 0], ["Liverpool", 9], ["Liverpool", 10], ["Liverpool", 8]]}]'''
 
         print("input:\n", test_in)
         fever_format = json.loads(test_in)
-        snli_format_instances = convert(fever_format, prependlinum=args.prependlinum, prependtitle=args.prependtitle, use_ir_prediction=args.use_ir_pred)
+        snli_format_instances = convert(fever_format, prependlinum=args.prependlinum, prependtitle=args.prependtitle, use_ir_prediction=args.use_ir_pred, n_sentences=args.n_sentences)
         print("\noutput:\n", json.dumps(snli_format_instances, indent=4))
 
     else:
@@ -164,5 +204,5 @@ if __name__ == "__main__":
         keyerr_count = 0
 
         instances = read_jsonl(args.src)
-        snli_format_instances = convert(instances, prependlinum=args.prependlinum, prependtitle=args.prependtitle, use_ir_prediction=args.use_ir_pred)
+        snli_format_instances = convert(instances, prependlinum=args.prependlinum, prependtitle=args.prependtitle, use_ir_prediction=args.use_ir_pred, n_sentences=args.n_sentences)
         save_jsonl(snli_format_instances, args.tar)
