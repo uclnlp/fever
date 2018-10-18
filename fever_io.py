d920d4fbc63b102a8c15f9030bd4656b0bc0fa36
Jeff Mitchell <mitchelljeff@hotmail.com>
2018-10-18 11:40:16 +0100

    fixed a bug in fever_io

--- fever_io.py
+++ fever_io.py
@@ -108,7 +108,7 @@ def titles_to_jsonl_num(wikipedia_dir="data/wiki-pages/wiki-pages/", doctitles="
                         lines=data["lines"]
                         if lines != "":
                             w.write(title+"\t"+jnum+"\t"+str(point)+"\n")
-                        t2jnum[title]=(jnum,point)
+                            t2jnum[title]=(jnum,point)
                         point=f.tell()
                         line=f.readline()
     return t2jnum
