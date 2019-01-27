f3ed50c2dfb7a32d9d8ea2afcd5d0fda7d13c884
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2019-01-26 23:13:03 -0600

    fix path

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@
 
 wget http://tti-coin.jp/data/yoneda/fever/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip -O results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip
 wget http://tti-coin.jp/data/yoneda/fever/data.zip -O data.zip
-unzip results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip -d result
+unzip results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip -d results
 unzip data
