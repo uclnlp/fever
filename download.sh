bb94123f9a432e57845151fa93ed67adf5a718b9
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2019-01-27 00:04:52 -0600

    create directory before wget

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+mkdir results
 wget http://tti-coin.jp/data/yoneda/fever/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip -O results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip
 wget http://tti-coin.jp/data/yoneda/fever/data.zip -O data.zip
 unzip results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip -d results
