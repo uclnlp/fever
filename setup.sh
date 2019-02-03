186e03ec11a876d829ff7f9508b2a581c165ea36
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2019-02-02 21:47:08 -0600

    add replacement for model file

--- setup.sh
+++ setup.sh
@@ -9,8 +9,14 @@ echo "replacing **BASE_DIR** with ${PARENT_DIR}"
 # escape slashes
 ESCAPED_PARENT_DIR=$(echo ${PARENT_DIR}| sed 's/\//\\\//g')
 
+CONFIG1=${PARENT_DIR}/fever/configs/submission_config.json
+CONFIG2=${PARENT_DIR}/fever/configs/base_config.json
+PYFILE=${PARENT_DIR}/fever/pipeline.py
+MODEL1=${PARENT_DIR}/fever/results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission/reader/checkpoint
+MODEL2=${PARENT_DIR}/fever/results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission/reader/shared_resources/config.yaml
+
 # replace
-sed -i -e "s/\*\*BASE_DIR\*\*/${ESCAPED_PARENT_DIR}/g" ${PARENT_DIR}/fever/configs/submission_config.json ${PARENT_DIR}/fever/configs/base_config.json ${PARENT_DIR}/fever/pipeline.py
+sed -i -e "s/\*\*BASE_DIR\*\*/${ESCAPED_PARENT_DIR}/g" ${CONFIG1} ${CONFIG2} ${PYFILE} ${MODEL1} ${MODEL2}
 
 
 
