ca18f03ce3f8b143c0ba2ece73c490cc6c90ce0c
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2019-02-04 00:09:38 -0600

    remove downloadl.sh

--- download.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir results
-wget http://tti-coin.jp/data/yoneda/fever/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip -O results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip
-wget http://tti-coin.jp/data/yoneda/fever/data.zip -O data.zip
-unzip results/base+sampling2+evscores+rerank+train+dev+test-shared_test.ver0727_newaggr_submission.zip -d results
-unzip data
